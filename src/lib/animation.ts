6bbd293dfb068eed8e33f268e9d45ff2095472c0
Moritz Reich <moritz.reich@minest.de>
2022-10-02 11:01:36 +0200

    improved animation

--- src/lib/animation.ts
+++ src/lib/animation.ts
@@ -1,26 +1,25 @@
 export const animateCSS = (
   element: HTMLElement,
   animation: string,
+  duration = "0.5s",
   prefix = "animate__"
 ) =>
   // We create a Promise and return it
   new Promise((resolve, _) => {
     const animationName = `${prefix}${animation}`;
-    const node = element;
 
-    if (node) {
-      node.classList.add(`${prefix}animated`, animationName);
+    element.classList.add(`${prefix}animated`, animationName);
+    element.style.setProperty("--animate-duration", duration);
 
-      const handleAnimationEnd: EventListenerOrEventListenerObject = (
-        event
-      ) => {
-        event.stopPropagation();
-        node.classList.remove(`${prefix}animated`, animationName);
-        resolve("Animation ended");
-      };
+    const handleAnimationEnd: EventListenerOrEventListenerObject = (event) => {
+      event.stopPropagation();
+      element.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    };
 
-      // When the animation ends, we clean the classes and resolve the Promise
+    // When the animation ends, we clean the classes and resolve the Promise
 
-      node.addEventListener("animationend", handleAnimationEnd, { once: true });
-    }
+    element.addEventListener("animationend", handleAnimationEnd, {
+      once: true,
+    });
   });
